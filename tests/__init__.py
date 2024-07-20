5089e4b87b64793108067d53cf3f23c334284732
Rakesh R <106605109+Dr-Rakesh@users.noreply.github.com>
2024-07-21 00:52:37 +0530

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,13 @@
+import logging
+from .utils import setup_logging
 
+# Set up logging for the application
+setup_logging(logging.INFO)
+
+# Import the main app instance to make it available at the package level
+from .main import app
+
+# You can also include any package-level constants, variables, or initializations here
+# For example:
+APP_NAME = "My FastAPI Web Extractor"
+VERSION = "1.0.0"
